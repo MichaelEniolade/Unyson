bd158c9e6459ce0de4484ec61742f7c2ace73d70
Gheorghe <pisarenco.gh@gmail.com>
2017-06-19 09:30:32 +0300

    Fixed #2632

--- framework/includes/option-types/slider/static/js/scripts.js
+++ framework/includes/option-types/slider/static/js/scripts.js
@@ -6,7 +6,11 @@
 	fwEvents.on('fw:options:init', function (data) {
 		data.$elements.find('.fw-option-type-slider:not(.initialized)').each(function () {
 			var options = JSON.parse($(this).attr('data-fw-irs-options'));
+      if (options.max <= 10) {
+        options.grid_num = options.max - 1
+      }
 			var slider = $(this).find('.fw-irs-range-slider').ionRangeSlider(_.defaults(options, defaults));
+			console.log( options );
 		}).addClass('initialized');
 	});
 
